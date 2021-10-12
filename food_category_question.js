4b368fa40d498ee195589880a1c6b3033fcf6f46
Anand Pattabiraman <andymochi@gmail.com>
2021-10-11 17:53:17 -0700

    Finished adding food_category_question question 2 on the food quiz!

---
+++ food_category_question.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+class FoodCategoryQuestion extends React.Component {
+  correctAnswer = 0;
+  answerTexts = ["Beef", "Shrimp", "Chicken", "Tofu"];
+
+  constructor(props) {
+    super(props);
+  }
+
+  _renderInput = (i, isSelected, anyAnswerSelected) => {
+    const classNameIsSelected = isSelected ? "FoodCategoryQuestion-input--selected" : "";
+    const classNameShowCorrect = (anyAnswerSelected && i === this.correctAnswer) ? "FoodCategoryQuestion-input--showCorrect" : "";
+    const classNameShowIncorrect = (isSelected && i !== this.correctAnswer) ? "FoodCategoryQuestion-input--showIncorrect" : "";
+
+    return e('div', {
+      className: "FoodCategoryQuestion-input " + classNameIsSelected + " " + classNameShowCorrect + " " + classNameShowIncorrect
+    },
+      e('div', {
+        className: "FoodCategoryQuestion-inputContent"
+      }, `${this.answerTexts[i]}`)
+    );
+  }
+
+  _renderAnswer = (i) => {
+    const correctOrIncorrectText = (i === this.correctAnswer) ? "Correct!" : "Mooooo. The steaks are high.";
+    return e('div', { className: "FoodCategoryQuestion-answer" }, `${correctOrIncorrectText} It costs 60 kg CO2 to produce 1 kg of beef, compared to 12 kg CO2 for 1kg of shrimp, 6 kg CO2 for 1 kg of chicken, and 3 kg CO2 for 1 kg of tofu. Take a look at the carbon footprints of various foods - [insert graph here]. In general, eating less meat and more plant-based foods (think beans, tofu, vegetables) will significantly reduce your carbon footprint.` );
+  }
+
+  render() {
+    return e(PopUpQuestion, {
+      className: "FoodCategoryQuestion",
+      inputsContainerClassName: "FoodCategoryQuestion-inputsContainer",
+      numInputs: 4,
+      questionText: "2. Which of the following foods contributes most to carbon emissions?",
+      renderInput: this._renderInput,
+      renderAnswer: this._renderAnswer,
+    });
+  }
+}
+
+(() => {
+  const domContainer = document.querySelector('#food_category_question');
+  ReactDOM.render(e(FoodCategoryQuestion), domContainer);
+})()
